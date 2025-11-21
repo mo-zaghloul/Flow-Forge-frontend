cc56d7251b6f61cd80a691f1f717932f00290cff
mzaghloul288 <mzaghloul288@gmail.com>
2025-11-21 18:54:07 +0000

    featuers sidebar

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Sidebar from "./components/sidebar";
+import FlowDemo from "./FlowDemo";
+
+export default function Home() {
+  return (
+    <div className="flex w-screen h-screen bg-black screen">
+      {/* Sidebar على الشمال */}
+      <Sidebar />
+
+      {/* محتوى الصفحة */}
+      <main className="flex-1 flex flex-col items-center justify-center gap-6 overflow-auto p-4">
+        <FlowDemo />
+        
+      </main>
+    </div>
+  );
+}
